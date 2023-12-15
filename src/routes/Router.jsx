6924e7faf5d30c4a00d34f2cb21b8acc9164b88e
ichiro-ss <sei.swmr@gmail.com>
2023-12-15 17:20:44 +0900

    browser router to hash router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux'; // eslint-disable-line import/no-extraneous-dependencies
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // eslint-disable-line import/no-extraneous-dependencies
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'; // eslint-disable-line import/no-extraneous-dependencies
 import { Home } from '../pages/Home';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
@@ -9,13 +9,13 @@ import { NotFound } from '../pages/NotFound';
 export const Router = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         {auth ? <Route path="/" element={<Home />} /> : <Route path="*" element={<Navigate to="/signin" />} />}
         <Route element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
