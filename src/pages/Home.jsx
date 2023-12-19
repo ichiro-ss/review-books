087e928db7d8c9f28cdc79df8e26a0f6cedaaf5a
ichiro-ss <sei.swmr@gmail.com>
2023-12-20 01:57:59 +0900

    offset

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,16 @@ export const Home = () => {
   const [cookies] = useCookies();
   const [books, setBooks] = useState([]);
   const [errorMessage, setErrorMessage] = useState([]);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
     axios
       .get(`${url}/books`, {
         headers: {
           authorization: `Bearer ${cookies.token}`,
+        },
+        params: {
+          offset,
         },
       })
       .then((res) => {
@@ -24,13 +28,32 @@ export const Home = () => {
       .catch((err) => {
         setErrorMessage(`本データの取得に失敗しました．${err}`);
       });
-  }, []);
+  }, [offset]);
+
+  const handleNext = () => {
+    setOffset((prevOffset) => prevOffset + 10);
+  };
+
+  const handlePrev = () => {
+    if (offset >= 10) {
+      setOffset((prevOffset) => prevOffset - 10);
+    }
+  };
+
   return (
     <div className="book-list">
       <Header />
       <p className="error-msg">{errorMessage}</p>
       <h2>本一覧</h2>
       <BooksTable books={books} />
+      <div className="book-list__btn-container">
+        <button className="book-list__prev-btn" type="button" onClick={handlePrev}>
+          前
+        </button>
+        <button className="book-list__next-btn" type="button" onClick={handleNext}>
+          次
+        </button>
+      </div>
     </div>
   );
 };
