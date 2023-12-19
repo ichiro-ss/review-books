28659a509a2b62d64c07eddfd2f1a4c32047d13e
ichiro-ss <sei.swmr@gmail.com>
2023-12-19 15:49:25 +0900

    get books reviews

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 import { Header } from '../components/Header';
+import { url } from '../const';
 
 export const Home = () => {
+  const [cookies] = useCookies();
+  const [books, setBooks] = useState([]);
+  const [errorMessage, setErrorMessage] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${url}/books`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((err) => {
+        setErrorMessage(`本データの取得に失敗しました．${err}`);
+      });
+  }, []);
   return (
     <>
       <Header />
-      <h2>Home</h2>
+      <p className="error-message">{errorMessage}</p>
+      <h2>本一覧</h2>
+      <ul className="books-list">
+        {books.map((book, key) => {
+          return (
+            <li key={book.id} className="book-review">
+              {book.title}: {book.review}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
