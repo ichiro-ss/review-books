c306ffd0f4a922b9c0c176774871866a0f6f9392
ichiro-ss <sei.swmr@gmail.com>
2023-12-20 02:44:58 +0900

    display username

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,7 @@ export const Header = () => {
       {auth ? (
         <>
           <img src={cookies.iconUrl} alt="" />
+          <strong>{cookies.name}</strong>
           <button type="button" onClick={handleSignOut} className="sign-out-button">
             サインアウト
           </button>
