c65662774fe972eb1a692ac20631d58210200c52
ichiro-ss <sei.swmr@gmail.com>
2023-12-21 00:54:33 +0900

    routing

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux'; // eslint-disable-line import/no-extraneous-dependencies
 import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'; // eslint-disable-line import/no-extraneous-dependencies
 import { Home } from '../pages/Home';
 import { SignIn } from '../pages/SignIn';
@@ -6,11 +7,12 @@ import { SignUp } from '../pages/SignUp';
 import { NotFound } from '../pages/NotFound';
 
 export const Router = () => {
+  const auth = useSelector((state) => state.auth.isSignIn);
   return (
     <HashRouter>
       <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={auth ? <Navigate replace to="/" /> : <SignIn />} />
+        <Route path="/signup" element={auth ? <Navigate replace to="/" /> : <SignUp />} />
         <Route path="/" element={<Home />} />
         <Route element={<NotFound />} />
       </Routes>
