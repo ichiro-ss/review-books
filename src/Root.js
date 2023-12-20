3cf9a172eb571168f2fd4aec0d8e6908700e6e41
ichiro-ss <sei.swmr@gmail.com>
2023-12-21 03:00:30 +0900

    remove Root.js

--- src/Root.js
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import { Provider } from 'react-redux'; // eslint-disable-line import/no-extraneous-dependencies
-// import { store } from './store';
-
-// export const Root = ({ children }) => {
-//   return <Provider store={store}>{children}</Provider>;
-// };
