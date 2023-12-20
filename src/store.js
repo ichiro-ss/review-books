db82212b6ff29764227a384eaabd7a1d66238cb6
ichiro-ss <sei.swmr@gmail.com>
2023-12-21 02:59:41 +0900

    remove store.js

--- src/store.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from '@reduxjs/toolkit'; // eslint-disable-line import/no-extraneous-dependencies
-// import { authSlice } from './authSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     auth: authSlice.reducer,
-//   },
-// });
