bb72276ae7d4ecd9fca777bc049f72bf1daaf9be
ichiro-ss <sei.swmr@gmail.com>
2023-12-21 15:12:06 +0900

    link to profile page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,6 +20,9 @@ export const Header = () => {
         <>
           <img src={cookies.iconUrl} alt="" />
           <strong>{cookies.name}</strong>
+          <Link className="header__edit-user" to="/profile">
+            ユーザー情報編集
+          </Link>
           <button type="button" onClick={handleSignOut} className="sign-out-button">
             サインアウト
           </button>
