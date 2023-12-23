d802d47d25487327560c6b810c0ea74fe37ebf7c
ichiro-ss <sei.swmr@gmail.com>
2023-12-23 13:30:49 +0900

    loading on edit page

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import ClipLoader from 'react-spinners/ClipLoader'; // eslint-disable-line import/no-extraneous-dependencies
 import { Header } from '../components/Header';
 import { url } from '../const';
 
@@ -12,8 +13,10 @@ export const Edit = () => {
   const [cookies, setCookie, removeCookie] = useCookies();
   const [errorMessage, setErrorMessage] = useState();
   const [book, setBook] = useState([]);
+  const [pageLoading, setPageLoading] = useState(false);
 
   useEffect(() => {
+    setPageLoading(true);
     axios
       .get(`${url}/books/${params.id}`, {
         headers: {
@@ -21,13 +24,15 @@ export const Edit = () => {
         },
       })
       .then((res) => {
-        setBook(res.data);
         if (!res.data.isMine) {
           navigate(`/detail/${params.id}`);
         }
+        setBook(res.data);
+        setPageLoading(false);
       })
       .catch((err) => {
         setErrorMessage(`本データの取得に失敗しました．${err}`);
+        setPageLoading(false);
       });
   }, []);
 
@@ -37,7 +42,19 @@ export const Edit = () => {
         <Header />
         <p className="error-msg">{errorMessage}</p>
         <h1>Edit</h1>
-        {book.id}
+        {pageLoading ? (
+          <div className="loading custom-loader">
+            <ClipLoader color="blue" size={50} aria-label="Loading Spinner" data-testid="loader" />
+          </div>
+        ) : (
+          <div className="book-detail">
+            <div className="book-detail__title">{book.title}</div>
+            <div className="book-detail__url">{book.url}</div>
+            <div className="book-detail__detail">{book.detail}</div>
+            <div className="book-detail__review">{book.review}</div>
+            <div className="book-detail__reviewer">{book.reviewer}</div>
+          </div>
+        )}{' '}
       </main>
     </div>
   );
