0d10855d36311cd354a90a5c74170dcb69b2a251
ichiro-ss <sei.swmr@gmail.com>
2023-12-23 11:45:12 +0900

    edit page view param

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,11 +1,15 @@
+import { useParams } from 'react-router-dom';
 import { Header } from '../components/Header';
 
 export const Edit = () => {
+  const params = useParams();
+
   return (
     <div>
       <main className="edit">
         <Header />
         <h1>Edit</h1>
+        {params.id}
       </main>
     </div>
   );
